f195e67217d3b8a7820975c4d3f6059f5fe31855
amitli <amitli@rafael.co.il>
2026-06-11 13:03:00 +0300

    Enhance model evaluation and results reporting in validation process

--- Code_Train_B/run_model.py
+++ Code_Train_B/run_model.py
@@ -1,7 +1,6 @@
 from tqdm                             import tqdm
 
 from Code_Train_B.BoundingBoxMatcher import BoundingBoxMatcher
-from Code_Train_B.siglip2_model import Siglip2Model
 from Code_Train_B.vlm_model           import VlmModel
 from PIL                              import Image, ImageDraw, ImageFont
 import pandas as pd
@@ -45,63 +44,75 @@ class ModelResult:
         df.to_pickle(pkl_name)
 
 
+
+
+
 def calc_statistics(boundingBoxMatcher, df):
 
     jpg_file       = df['jpg_file'].values[0]
+
+    if jpg_file == '1_524400_186_14-51-38.jpg':
+        None
+
     gt_count       = df['gt_count'].values[0]
     l_gt_bb        = df['gt_bb'].values[0]
     l_gt_family    = df['gt_family'].values[0]
     l_model_family = df['model_family'].values[0]
     l_model_bb     = df['model_bb'].values[0]
 
+    res_array_size     = max(len(l_gt_bb), len(l_model_bb))
     matching_results   = boundingBoxMatcher.match(l_model_bb, l_gt_bb)
 
-    bb_count = 0
-    gt_tank_count = 0
-    gt_anti_aircraft_count = 0
-    gt_Launchers_count = 0
-    model_tank_count= 0
-    model_anti_aircraft_count = 0
-    model_Launchers_count = 0
+    l_res_gt_family    = l_gt_family
+    l_res_model_family = ['Not_Found'] * len(l_gt_family)
 
+    bb_count          = 0
+    l_tmp_gt_index    = []
+    l_tmp_model_index = []
+    l_tmp_model_fam   = []
+    l_tmp_bb_found    = [0] * len(l_model_bb)
     for matcing in matching_results:
-        score          = matcing["score"]
         match_gt_index = matcing["matched_gt_index"]
-        model_index    = matcing["model_index"]
-
         if match_gt_index != -1:
             bb_count     = bb_count + 1
-            gt_family    = l_gt_family[match_gt_index]
+            model_index  = matcing["model_index"]
             model_family = l_model_family[model_index].replace('vehicle', '').strip()
 
-            if gt_family == "Tank":
-                gt_tank_count = gt_tank_count + 1
-            elif gt_family == "Launchers":
-                gt_Launchers_count = gt_Launchers_count + 1
-            elif gt_family == "Anti aircraft":
-                gt_anti_aircraft_count = gt_anti_aircraft_count + 1
-            if gt_family == model_family:
-                if model_family == "Tank":
-                    model_tank_count = model_tank_count + 1
-                elif model_family == "Launchers":
-                    model_Launchers_count = model_Launchers_count + 1
-                elif model_family == "Anti aircraft":
-                    model_anti_aircraft_count = model_anti_aircraft_count + 1
+            l_tmp_gt_index   .append(match_gt_index)
+            l_tmp_model_index.append(model_index)
+            l_tmp_model_fam  .append(model_family)
+
+            l_tmp_bb_found[model_index] = 1
+
+    # --- fill the founded gt wtih the model family
+    for i in range(len(l_tmp_gt_index)):
+        gt_index    = l_tmp_gt_index   [i]
+        model_fam   = l_tmp_model_fam  [i]
+        l_res_model_family[gt_index] = model_fam
+
+
+    # fill the rest of the model
+    for model_index in range(len(l_tmp_bb_found)):
+        if l_tmp_bb_found[model_index] == 0:
+            l_res_gt_family   .append("NOT_IN_GT")
+            model_pred  = l_model_family[model_index]
+            model_pred  = model_pred.rstrip()
+            l_res_model_family.append(model_pred)
+
+    l_res_bb_count    = [bb_count] + [0] * ( len(l_res_model_family)-1)
+    l_res_gt_count    = [gt_count] + [0] * (len(l_res_model_family) - 1)
+    l_jpg_res         = [jpg_file] * len(l_res_model_family)
+
 
     df_results = pd.DataFrame({
-                              "jpg_file"     : [jpg_file],
-                              "bb_count"     : [bb_count],
-                              "gt_count"     : [gt_count],
-                              "bb_score"     : [bb_count/gt_count],
-                              "gt_tank_count"            : [gt_tank_count],
-                              "gt_anti_aircraft_count"   : [gt_anti_aircraft_count],
-                              "gt_Launchers_count"       : [gt_Launchers_count],
-                              "model_tank_count"         : [model_tank_count],
-                              "model_anti_aircraft_count": [model_anti_aircraft_count],
-                              "model_Launchers_count"    : [model_Launchers_count],
+                              "jpg_file"        : l_jpg_res,
+                              "bb_count"        : l_res_bb_count,
+                              "gt_count"        : l_res_gt_count,
+                              "gt_family"       : l_res_gt_family,
+                              "model_family"    : l_res_model_family
     })
-
     return df_results
+
 
 def draw_result(df, full_path):
 
@@ -167,39 +178,63 @@ def draw_result(df, full_path):
     img.show()
 
 def print_statisics(df):
-    bb_count = df['bb_count'].sum()
-    gt_count = df['gt_count'].sum()
-    gt_tank_count = df['gt_tank_count'].sum()
-    gt_anti_aircraft_count = df['gt_anti_aircraft_count'].sum()
-    gt_Launchers_count = df['gt_Launchers_count'].sum()
-    model_tank_count = df['model_tank_count'].sum()
-    model_anti_aircraft_count = df['model_anti_aircraft_count'].sum()
-    model_Launchers_count = df['model_Launchers_count'].sum()
+    from sklearn.metrics import confusion_matrix
 
-    print(f"Total files: {len(df)}")
-    print("")
-    print(f'Bounding Box Count   : {bb_count}')
-    print(f'Bounding Box GT      : {gt_count}')
-    print(f'Bounding Box Accuracy: {(bb_count/gt_count):.2f}')
+    bb_count = df.bb_count.sum()
+    gt_count = df.gt_count.sum()
+    print(f"BB: {bb_count} | {gt_count}, {(bb_count/gt_count):.2f})%")
+    l_gt_fam    = df.gt_family.values
+    l_model_fam = df.model_family.values
 
-    print("")
-    print(f"Tank                  : {model_tank_count:2.0f} | {gt_tank_count:2.0f} Accuracy: {(model_tank_count/gt_tank_count):.2f}")
-    print(f"Anti aircraft Accuracy: {model_anti_aircraft_count} | {gt_anti_aircraft_count:2.0f} Accuracy: {(model_anti_aircraft_count / gt_anti_aircraft_count):.2f}")
-    print(f"Anti aircraft Accuracy: {model_Launchers_count} | {gt_Launchers_count:2.0f} Accuracy: {(model_Launchers_count / gt_Launchers_count):.2f}")
+    print(set(l_gt_fam))
 
+    y_true = l_gt_fam
+    y_pred = l_model_fam
+
+    # Get confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+
+    # Convert to percentages by row (each true class sums to 100%)
+    cm_pct = cm.astype(float) / cm.sum(axis=1, keepdims=True) * 100
+
+    # Pretty print
+    labels = sorted(set(y_true) | set(y_pred))
+    df = pd.DataFrame(cm_pct, index=labels, columns=labels)
+
+    print(df.round(2))
+
+
+def get_results(pkl_result_file):
+
+    boundingBoxMatcher   = BoundingBoxMatcher(threshold=0.8, criterion='iog')
+    l_statistics_results = []
+    df                   = pd.read_pickle(pkl_result_file)
+
+    for i in range(len(df)):
+        df_current = df[df.jpg_file == df.jpg_file[i]]
+        # draw_result(df_current, f'{jpg_files_path}/Images')
+        # if df_current.jpg_file.values[0] == '2_924400_1136_09-56-59.jpg':
+        #     print("")
+        statistics_results = calc_statistics(boundingBoxMatcher, df_current)
+        l_statistics_results.append(statistics_results)
+
+    df_statistics_results = pd.concat(l_statistics_results)
+    print_statisics(df_statistics_results)
+    exit(0)
 
 if __name__ == "__main__":
 
-# 1_564400_419_14-50-09.jpg
-# 1_564400_137_14-50-09.jpg
-#  1_524400_420_14-51-3.jpg
-
     RUN_MODEL   = False
     DB_TYPE     = "Validation" # "Train" / "Validation"
+    USE_MOLMO   = True
+
+    molmo_fname = ""
+    if USE_MOLMO:
+        molmo_fname = "_molmo"
 
     if DB_TYPE == "Train":
         jpg_files_path    = '/home/amitli/datasets/DOR_6/Train_B/Database'
-        pkl_result_file   = f'/home/amitli/repo/dor6_vision/Code_Train_B/Pickles/train_results.pkl'
+        pkl_result_file   = f'/home/amitli/repo/dor6_vision/Code_Train_B/Pickles/train_results{molmo_fname}.pkl'
         pkl_input_db_file = '/home/amitli/repo/dor6_vision/Code_Train_B/Pickles/train_db.pkl'
     else:
 
@@ -207,44 +242,27 @@ if __name__ == "__main__":
 
 
         jpg_files_path    = '/home/amitli/datasets/DOR_6/Train_B/validation'
-        pkl_result_file   = f'/home/amitli/repo/dor6_vision/Code_Train_B/Pickles/validation_results.pkl'
+        pkl_result_file   = f'/home/amitli/repo/dor6_vision/Code_Train_B/Pickles/validation_results{molmo_fname}.pkl'
         pkl_input_db_file = '/home/amitli/repo/dor6_vision/Code_Train_B/Pickles/validation_db.pkl'
 
-    boundingBoxMatcher   = BoundingBoxMatcher(threshold=0.8, criterion='iog')
-    l_statistics_results = []
     if RUN_MODEL is False:
-        df = pd.read_pickle(pkl_result_file)
-        for i in range(len(df)):
-            df_current = df[df.jpg_file == df.jpg_file[i]]
-            #draw_result(df_current, f'{jpg_files_path}/Images')
-            if df_current.jpg_file.values[0] == '2_924400_1136_09-56-59.jpg':
-                print("")
-            statistics_results = calc_statistics(boundingBoxMatcher, df_current)
-            l_statistics_results.append(statistics_results)
+       get_results(pkl_result_file)
 
-        df_statistics_results = pd.concat(l_statistics_results)
-        print_statisics(df_statistics_results)
-        exit(0)
-
-
-
-    df                      = pd.read_pickle(pkl_input_db_file)
-    df                      = df[df['num_gt'] > 0]
-    df                      = df.sample(n=50)
+    # df                      = pd.read_pickle(pkl_input_db_file)
+    # df                      = df[df['num_gt'] > 0]
+    # df                      = df.sample(n=50)
+    # df.to_pickle(pkl_input_db_file.replace('.pkl', '_50_samples.pkl'))
+    df                      = pd.read_pickle(pkl_input_db_file.replace('.pkl', '_50_samples.pkl'))
     modelResult             = ModelResult()
 
-    vlmModel     = VlmModel()
-    siglip2Model = Siglip2Model()
+    vlmModel     = VlmModel(use_molmo_crop=USE_MOLMO)
     for i in tqdm(range(len(df))):
         jpg_file       = df.jpg_file.values[i]
-
-        # if jpg_file != '1_564400_8_14-50-09.jpg':
-        #     continue
-
         l_gt_targets   = df.targets.values[i]
         l_gt_bb        = df.bb.values[i]
         full_jpg_file  = f'{jpg_files_path}/Images/{jpg_file}'
         l_model_bb_res = vlmModel.get_list_of_bounding_boxes(full_jpg_file)
+        l_model_bb_res = vlmModel.convert_bb_molmo_to_gemma4(l_model_bb_res, radius=50)
         l_crop_ratio   = vlmModel.create_crop_files(full_jpg_file, l_model_bb_res, min_crop_size=128)
         family_result  = vlmModel.classify_family_objects(len(l_model_bb_res))
 
--- Code_Train_B/vlm_model.py
+++ Code_Train_B/vlm_model.py
@@ -4,18 +4,20 @@ from openai import OpenAI
 import base64
 import json
 import os
-
+import re
 
 class VlmModel():
 
-    def __init__(self):
+    def __init__(self, use_molmo_crop=False):
         self.MODEL    = "gemma-4-31b-it"
         self.BASE_URL = "http://localhost:9000/v1"
         self.client   = OpenAI(
             api_key  = "gpustack_a853c8a4cf87ee4b_6d6d0ade6d71fbadf5b015e04fb5e825",
             base_url = "http://10.53.160.148/v1"
         )
+        self.molmo_client     = OpenAI(api_key="EMPTY", base_url="http://localhost:9100/v1")
         self.TMP_FILES_FOLDER = '/home/amitli/repo/dor6_vision/Code_Train_B/TMP_FOLDER/'
+        self.use_molmo_crop = use_molmo_crop
 
 
     def _encode_image(self, path):
@@ -46,7 +48,81 @@ class VlmModel():
 
         return messages
 
+    def molmo_get_all_points_prompt(self, target_img):
+
+        def encode_image(path):
+            with open(path, "rb") as f:
+                return base64.b64encode(f.read()).decode("utf-8")
+
+        messages = [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text",
+                     "text": "Locate all the military vehicles in this image. Return only their locations"},
+                    {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{encode_image(target_img)}"}}
+                ],
+            }
+        ]
+        return messages
+
+    def extract_molmo_coords(self, text):
+
+        # Regex searches for the content inside 'coords="..."'
+        match = re.search(r'coords="([\d\s]+)"', text)
+
+        if match:
+            # Get the string of numbers
+            coord_string = match.group(1)
+            # Split by whitespace and convert to integers
+            coords = [int(c) for c in coord_string.split()]
+            return coords
+
+        return []
+
+    def molmo_get_list_of_points(self, full_image_path):
+        messages = self.molmo_get_all_points_prompt(full_image_path)
+        response = self.molmo_client.chat.completions.create(
+            model="allenai/Molmo2-4B",
+            messages=messages,
+            temperature=0.0,
+            top_p=1,
+            seed=42,
+        )
+        res_text = response.choices[0].message.content
+        res_text = self.extract_molmo_coords(res_text)
+        # res_json = convert_point_to_boxes(res_text)
+
+        return res_text
+
+    def convert_bb_molmo_to_gemma4(self, l_model_bb_res, radius):
+        if self.use_molmo_crop is False:
+            return l_model_bb_res
+
+        l_gemma_results = []
+
+        for i in range(1, len(l_model_bb_res), 3):
+            point_id = l_model_bb_res[i]
+            x        = l_model_bb_res[i + 1]
+            y        = l_model_bb_res[i + 2]
+
+            l_gemma_results.append({
+                "id": point_id,
+                "box_2d": [
+                    y - radius,  # ymin
+                    x - radius,  # xmin
+                    y + radius,  # ymax
+                    x + radius,  # xmax
+                ]
+            })
+
+        return l_gemma_results
+
     def get_list_of_bounding_boxes(self, full_image_path):
+
+        if self.use_molmo_crop:
+            return self.molmo_get_list_of_points(full_image_path)
+
         messages = self._get_all_bb(full_image_path)
         schema = {
             "type": "object",
@@ -100,6 +176,7 @@ class VlmModel():
             res_json = {}
             print(f"Cant get bounding boxes, File: {os.path.basename(full_image_path)}, Got: {res_text}")
         return res_json
+
 
     def create_crop_files(self, image_path, model_bb_json_res, min_crop_size):
 
@@ -246,7 +323,11 @@ if __name__ == "__main__":
 
     #jpg_full_file = '/home/amitli/datasets/DOR_6/Train_B/validation/Images/1_564400_419_14-50-09.jpg'
     #jpg_full_file = '/home/amitli/datasets/DOR_6/Train_B/validation/Images/1_564400_137_14-50-09.jpg'
-    jpg_full_file = '/home/amitli/datasets/DOR_6/Train_B/validation/Images/1_524400_420_14-51-38.jpg'
-    vlm_model     = VlmModel()
-    result        = vlm_model.get_list_of_bounding_boxes(jpg_full_file)
-    print(result)
+    jpg_full_file  = '/home/amitli/datasets/DOR_6/Train_B/validation/Images/1_524400_420_14-51-38.jpg'
+
+    vlm_model      = VlmModel(use_molmo_crop=True)
+    l_model_bb_res = vlm_model.get_list_of_bounding_boxes(jpg_full_file)
+    l_model_bb_res = vlm_model.convert_bb_molmo_to_gemma4(l_model_bb_res, radius=50)
+    l_crop_ratio   = vlm_model.create_crop_files(jpg_full_file, l_model_bb_res, min_crop_size=128)
+    print(l_model_bb_res)
+    print(l_crop_ratio)
