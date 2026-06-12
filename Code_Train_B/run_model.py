79b29af6ae63171f1205dc3cf80988e5ce7016e2
amitli <amitli@rafael.co.il>
2026-06-12 21:39:56 +0300

    Add validation results for molmo and Gemma4 crops

--- Code_Train_B/run_model.py
+++ Code_Train_B/run_model.py
@@ -1,117 +1,14 @@
 from tqdm                             import tqdm
-
-from Code_Train_B.BoundingBoxMatcher import BoundingBoxMatcher
+from Code_Train_B.BoundingBoxMatcher  import BoundingBoxMatcher
 from Code_Train_B.vlm_model           import VlmModel
 from PIL                              import Image, ImageDraw, ImageFont
+from sklearn.metrics                  import confusion_matrix
 import pandas as pd
+import numpy  as np
 import ast
 import json
 
 from app_config.settings import FONT_FILE
-
-
-class ModelResult:
-    def __init__(self):
-        self.l_jpg_file          = []
-        self.l_gt_count          = []
-        self.l_gt_bb             = []
-        self.l_gt_family         = []
-        self.l_model_bb          = []
-        self.l_model_family      = []
-        self.l_model_description = []
-
-    def add(self, jpg_file, l_model_bb, l_model_family, l_model_description):
-
-        l_gt_bb     = df[df.jpg_file == jpg_file].bb.values[0]
-        l_gt_family = df[df.jpg_file == jpg_file].family.values[0]
-
-        self.l_jpg_file          .append(jpg_file)
-        self.l_gt_count          .append(len(l_gt_bb))
-        self.l_gt_bb             .append(l_gt_bb)
-        self.l_gt_family         .append(l_gt_family)
-        self.l_model_bb          .append(l_model_bb)
-        self.l_model_family      .append(l_model_family)
-        self.l_model_description .append(l_model_description)
-
-    def save_to_pickle(self, pkl_name):
-        df = pd.DataFrame({'jpg_file'          : self.l_jpg_file,
-                           'gt_count'          : self.l_gt_count,
-                           'gt_bb'             : self.l_gt_bb,
-                           'gt_family'         : self.l_gt_family,
-                           'model_bb'          : self.l_model_bb,
-                           'model_family'      : self.l_model_family,
-                           'model_description' : self.l_model_description})
-        df.to_pickle(pkl_name)
-
-
-
-
-
-def calc_statistics(boundingBoxMatcher, df):
-
-    jpg_file       = df['jpg_file'].values[0]
-
-    if jpg_file == '1_524400_186_14-51-38.jpg':
-        None
-
-    gt_count       = df['gt_count'].values[0]
-    l_gt_bb        = df['gt_bb'].values[0]
-    l_gt_family    = df['gt_family'].values[0]
-    l_model_family = df['model_family'].values[0]
-    l_model_bb     = df['model_bb'].values[0]
-
-    res_array_size     = max(len(l_gt_bb), len(l_model_bb))
-    matching_results   = boundingBoxMatcher.match(l_model_bb, l_gt_bb)
-
-    l_res_gt_family    = l_gt_family
-    l_res_model_family = ['Not_Found'] * len(l_gt_family)
-
-    bb_count          = 0
-    l_tmp_gt_index    = []
-    l_tmp_model_index = []
-    l_tmp_model_fam   = []
-    l_tmp_bb_found    = [0] * len(l_model_bb)
-    for matcing in matching_results:
-        match_gt_index = matcing["matched_gt_index"]
-        if match_gt_index != -1:
-            bb_count     = bb_count + 1
-            model_index  = matcing["model_index"]
-            model_family = l_model_family[model_index].replace('vehicle', '').strip()
-
-            l_tmp_gt_index   .append(match_gt_index)
-            l_tmp_model_index.append(model_index)
-            l_tmp_model_fam  .append(model_family)
-
-            l_tmp_bb_found[model_index] = 1
-
-    # --- fill the founded gt wtih the model family
-    for i in range(len(l_tmp_gt_index)):
-        gt_index    = l_tmp_gt_index   [i]
-        model_fam   = l_tmp_model_fam  [i]
-        l_res_model_family[gt_index] = model_fam
-
-
-    # fill the rest of the model
-    for model_index in range(len(l_tmp_bb_found)):
-        if l_tmp_bb_found[model_index] == 0:
-            l_res_gt_family   .append("NOT_IN_GT")
-            model_pred  = l_model_family[model_index]
-            model_pred  = model_pred.rstrip()
-            l_res_model_family.append(model_pred)
-
-    l_res_bb_count    = [bb_count] + [0] * ( len(l_res_model_family)-1)
-    l_res_gt_count    = [gt_count] + [0] * (len(l_res_model_family) - 1)
-    l_jpg_res         = [jpg_file] * len(l_res_model_family)
-
-
-    df_results = pd.DataFrame({
-                              "jpg_file"        : l_jpg_res,
-                              "bb_count"        : l_res_bb_count,
-                              "gt_count"        : l_res_gt_count,
-                              "gt_family"       : l_res_gt_family,
-                              "model_family"    : l_res_model_family
-    })
-    return df_results
 
 
 def draw_result(df, full_path):
@@ -177,16 +74,118 @@ def draw_result(df, full_path):
 
     img.show()
 
-def print_statisics(df):
-    from sklearn.metrics import confusion_matrix
 
-    bb_count = df.bb_count.sum()
-    gt_count = df.gt_count.sum()
-    print(f"BB: {bb_count} | {gt_count}, {(bb_count/gt_count):.2f})%")
+class ModelResult:
+    def __init__(self):
+        self.l_jpg_file          = []
+        self.l_gt_count          = []
+        self.l_gt_bb             = []
+        self.l_gt_family         = []
+        self.l_model_bb          = []
+        self.l_model_family      = []
+        self.l_model_description = []
+
+    def add(self, jpg_file, l_model_bb, l_model_family, l_model_description):
+
+        l_gt_bb     = df[df.jpg_file == jpg_file].bb.values[0]
+        l_gt_family = df[df.jpg_file == jpg_file].family.values[0]
+
+        self.l_jpg_file          .append(jpg_file)
+        self.l_gt_count          .append(len(l_gt_bb))
+        self.l_gt_bb             .append(l_gt_bb)
+        self.l_gt_family         .append(l_gt_family)
+        self.l_model_bb          .append(l_model_bb)
+        self.l_model_family      .append(l_model_family)
+        self.l_model_description .append(l_model_description)
+
+    def save_to_pickle(self, pkl_name):
+        df = pd.DataFrame({'jpg_file'          : self.l_jpg_file,
+                           'gt_count'          : self.l_gt_count,
+                           'gt_bb'             : self.l_gt_bb,
+                           'gt_family'         : self.l_gt_family,
+                           'model_bb'          : self.l_model_bb,
+                           'model_family'      : self.l_model_family,
+                           'model_description' : self.l_model_description})
+        df.to_pickle(pkl_name)
+
+
+
+
+
+def calc_statistics(boundingBoxMatcher, df):
+
+    jpg_file       = df['jpg_file'].values[0]
+
+    # if jpg_file == '1_524400_186_14-51-38.jpg':
+    #     None
+
+    gt_count       = df['gt_count'].values[0]
+    l_gt_bb        = df['gt_bb'].values[0]
+    l_gt_family    = df['gt_family'].values[0]
+    l_model_family = df['model_family'].values[0]
+    l_model_bb     = df['model_bb'].values[0]
+
+    res_array_size     = max(len(l_gt_bb), len(l_model_bb))
+    matching_results   = boundingBoxMatcher.match(l_model_bb, l_gt_bb)
+
+    l_res_gt_family    = l_gt_family
+    l_res_model_family = ['Not_Found'] * len(l_gt_family)
+
+    bb_count          = 0
+    l_tmp_gt_index    = []
+    l_tmp_model_index = []
+    l_tmp_model_fam   = []
+    l_tmp_bb_found    = [0] * len(l_model_bb)
+    for matcing in matching_results:
+        match_gt_index = matcing["matched_gt_index"]
+        if match_gt_index != -1:
+            bb_count     = bb_count + 1
+            model_index  = matcing["model_index"]
+            model_family = l_model_family[model_index].replace('vehicle', '').strip()
+
+            l_tmp_gt_index   .append(match_gt_index)
+            l_tmp_model_index.append(model_index)
+            l_tmp_model_fam  .append(model_family)
+
+            l_tmp_bb_found[model_index] = 1
+
+    # --- fill the founded gt wtih the model family
+    for i in range(len(l_tmp_gt_index)):
+        gt_index    = l_tmp_gt_index   [i]
+        model_fam   = l_tmp_model_fam  [i]
+        l_res_model_family[gt_index] = model_fam
+
+
+    # fill the rest of the model
+    for model_index in range(len(l_tmp_bb_found)):
+        if l_tmp_bb_found[model_index] == 0:
+            l_res_gt_family   .append("NOT_IN_GT")
+            model_pred  = l_model_family[model_index]
+            model_pred  = model_pred.rstrip()
+            l_res_model_family.append(model_pred)
+
+    l_res_bb_count    = [bb_count] + [0] * ( len(l_res_model_family)-1)
+    l_res_gt_count    = [gt_count] + [0] * (len(l_res_model_family) - 1)
+    l_jpg_res         = [jpg_file] * len(l_res_model_family)
+
+
+    df_results = pd.DataFrame({
+                              "jpg_file"        : l_jpg_res,
+                              "bb_count"        : l_res_bb_count,
+                              "gt_count"        : l_res_gt_count,
+                              "gt_family"       : l_res_gt_family,
+                              "model_family"    : l_res_model_family
+    })
+    return df_results
+
+
+def print_statisics_not_in_gt(df):
+    df          = df[df.gt_family == 'NOT_IN_GT']
     l_gt_fam    = df.gt_family.values
     l_model_fam = df.model_family.values
 
-    print(set(l_gt_fam))
+    print(f'l_gt_fam    = {set(l_gt_fam)}')
+    print(f'l_model_fam = {set(l_model_fam)}')
 
     y_true = l_gt_fam
     y_pred = l_model_fam
@@ -201,7 +200,48 @@ def print_statisics(df):
     labels = sorted(set(y_true) | set(y_pred))
     df = pd.DataFrame(cm_pct, index=labels, columns=labels)
 
+    df.index.name = "GT (Actual)"
+    df.columns.name = "Predicted"
     print(df.round(2))
+
+def print_statisics(df):
+
+    # 1. fillter and calc statiscs only on founded bouding boxes
+    df = df[df.model_family != 'Not_Found']
+
+    # show confustion matrix only on GT family:
+    df = df[df['gt_family'].isin(['Anti aircraft', 'Launchers', 'Tank'])]
+
+    bb_count = df.bb_count.sum()
+    gt_count = df.gt_count.sum()
+    print(f"BB: {bb_count} | {gt_count}, {(bb_count/gt_count):.2f})%")
+    l_gt_fam    = df.gt_family.values
+    l_model_fam = df.model_family.values
+
+    # unique_true = np.unique(l_gt_fam)
+    # unique_pred = np.unique(l_model_fam)
+    # print(f'unique_true = {unique_true}')
+    # print(f'unique_pred = {unique_pred}')
+
+    y_true = l_gt_fam
+    y_pred = l_model_fam
+
+    # Get confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+
+    # Convert to percentages by row (each true class sums to 100%)
+    cm_pct = cm.astype(float) / cm.sum(axis=1, keepdims=True) * 100
+
+    # Pretty print
+    labels = sorted(set(y_true) | set(y_pred))
+    df = pd.DataFrame(cm_pct, index=labels, columns=labels)
+
+    df.index.name = "GT (Actual)"
+    df.columns.name = "Predicted"
+    print(df.round(2))
+
+    # row - GT
+    # col - Model
 
 
 def get_results(pkl_result_file):
@@ -213,20 +253,20 @@ def get_results(pkl_result_file):
     for i in range(len(df)):
         df_current = df[df.jpg_file == df.jpg_file[i]]
         # draw_result(df_current, f'{jpg_files_path}/Images')
-        # if df_current.jpg_file.values[0] == '2_924400_1136_09-56-59.jpg':
-        #     print("")
         statistics_results = calc_statistics(boundingBoxMatcher, df_current)
         l_statistics_results.append(statistics_results)
 
     df_statistics_results = pd.concat(l_statistics_results)
     print_statisics(df_statistics_results)
+    #print_statisics_not_in_gt(df_statistics_results)
+
     exit(0)
 
 if __name__ == "__main__":
 
     RUN_MODEL   = False
     DB_TYPE     = "Validation" # "Train" / "Validation"
-    USE_MOLMO   = True
+    USE_MOLMO   = False
 
     molmo_fname = ""
     if USE_MOLMO:
